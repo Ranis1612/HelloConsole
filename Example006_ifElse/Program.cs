2fcdc122b36b662cbc0df322ba724c31e4244761
Ranis1612 <Ranis1612@mail.ru>
2023-01-29 17:54:23 +0500

    Example006_ifElse

---
+++ Example006_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "гульназ" ) // ToLower() - операция позволяющая перевести все символы в нижний регистр
+{
+  Console.WriteLine("Привет Любимая!!!");  
+}
+else
+{
+  Console.WriteLine("Привет, " );
+  Console.WriteLine(username);
+}
